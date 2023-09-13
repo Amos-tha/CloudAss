a81c6b736248dffb143ae46e6e85466527b19dc5
JingEn012 <113515442+JingEn012@users.noreply.github.com>
2023-09-13 23:17:39 +0800

    Display and Download (test3)

--- EmpApp.py
+++ EmpApp.py
@@ -89,7 +89,7 @@ def list_files():
     s3 = boto3.client('s3')
     contents = []
     for image in s3.list_objects(Bucket=custombucket)['Contents']:
-        contents.append(f'https://{custombucket}.s3.amazonaws.com/{image.key}')
+        contents.append(f'https://{custombucket}.s3.amazonaws.com/{image}')
 
     return contents
 
