ec367bf44d06fdb41f1760ee717a6bbf4393f636
KinWeng <32710027+hoKingwin@users.noreply.github.com>
2023-09-15 18:29:45 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-customhost = "http://itp.cbvkxab6tdeg.us-east-1.rds.amazonaws.com"
+customhost = "itp.cbvkxab6tdeg.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "itp"
