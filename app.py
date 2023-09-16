6ab55712c046c8b875817f7c6449458c303a3723
KinWeng <32710027+hoKingwin@users.noreply.github.com>
2023-09-16 15:09:26 +0800

    Offers

--- app.py
+++ app.py
@@ -1,83 +1,110 @@
-from flask import Flask, render_template, request, redirect
-from pymysql import connections
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    url_for,
+    session,
+    jsonify,
+    json,
+)
+from pymysql import connections, cursors
 import os
 import boto3
+from datetime import datetime 
 from config import *
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "amos-itp"
 
 bucket = custombucket
 region = customregion
 
 db_conn = connections.Connection(
-    host=customhost,
-    port=3306,
-    user=customuser,
-    password=custompass,
-    db=customdb
+    host=customhost, port=3306, user=customuser, password=custompass, db=customdb
 )
 output = {}
-table = 'employee'
+table = "employee"
 
-@app.route("/", methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template('Index.html')
+    return render_template("Index.html")
 
-@app.route("/about", methods=['POST'])
+
+@app.route("/about", methods=["POST"])
 def about():
-    return render_template('www.tarc.edu.my')
+    return render_template("www.tarc.edu.my")
 
-@app.route("/login", methods=['GET', 'POST'])
+
+@app.route("/login", methods=["GET", "POST"])
 def StudLogin():
-    return render_template('StudLogin.html')
+    return render_template("StudLogin.html")
 
-@app.route("/company/register", methods=['GET','POST'])
+
+@app.route("/company/register", methods=["GET", "POST"])
 def comp_register():
-    return render_template('RegisterComp.html')
+    return render_template("CompRegister.html")
 
-@app.route("/company/Register", methods=['GET','POST'])
+
+@app.route("/company/Register", methods=["GET", "POST"])
 def Comp_Register():
-    compName = request.form['inputName']
-    compEmail = request.form['inputEmail']
-    compPassword = request.form['inputPassword']
-    compPhoneNo = request.form['inputPhoneNumber']
-    compAddress = request.form['inputAddress']
-    compWebsite = request.form['inputWebsite']
-    socialMedia = request.form['inputSocialMedia']
+    compName = request.form["inputName"]
+    compEmail = request.form["inputEmail"]
+    compPassword = request.form["inputPassword"]
+    compPhoneNo = request.form["inputPhoneNumber"]
+    compAddress = request.form["inputAddress"]
+    compWebsite = request.form["inputWebsite"]
+    socialMedia = request.form["inputSocialMedia"]
     registerStatus = "pending"
     committeeID = None
-    compLogo = request.files['inputLogo']
-    businessLicense = request.files['inputLicense']
+    compLogo = request.files["inputLogo"]
+    businessLicense = request.files["inputLicense"]
 
     insert_sql = "INSERT INTO company (compName, compEmail, compPassword, compPhoneNo, compAddress, compWebsite, socialMedia, registerStatus, committeeID)VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
     cursor = db_conn.cursor()
 
     if compLogo.filename == "":
         return "Please select an image for logo"
-    
+
     if businessLicense.filename == "":
         return "Please select an image for license"
 
     try:
-        cursor.execute(insert_sql, (compName, compEmail, compPassword, compPhoneNo, compAddress, compWebsite, socialMedia, registerStatus, committeeID))
+        cursor.execute(
+            insert_sql,
+            (
+                compName,
+                compEmail,
+                compPassword,
+                compPhoneNo,
+                compAddress,
+                compWebsite,
+                socialMedia,
+                registerStatus,
+                committeeID,
+            ),
+        )
         db_conn.commit()
         compID = cursor.lastrowid
         # Uplaod image file in S3 #
         logo_file = "comp-id-" + str(compID) + "_logo"
         license_file = "comp-id-" + str(compID) + "_license"
-        s3 = boto3.resource('s3')
+        s3 = boto3.resource("s3")
 
         try:
             print("Data inserted in MySQL RDS... uploading image to S3...")
             s3.Bucket(custombucket).put_object(Key=logo_file, Body=compLogo)
             s3.Bucket(custombucket).put_object(Key=license_file, Body=businessLicense)
-            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
-            s3_location = (bucket_location['LocationConstraint'])
+            bucket_location = boto3.client("s3").get_bucket_location(
+                Bucket=custombucket
+            )
+            s3_location = bucket_location["LocationConstraint"]
 
             if s3_location is None:
-                s3_location = ''
+                s3_location = ""
             else:
-                s3_location = '-' + s3_location
+                s3_location = "-" + s3_location
 
         except Exception as e:
             return str(e)
@@ -85,26 +112,171 @@ def Comp_Register():
     finally:
         cursor.close()
 
-    return redirect("/")
+    return redirect(url_for("home"))
 
 
-@app.route("/company/login", methods=['GET','POST'])
+@app.route("/company/login", methods=["GET", "POST"])
 def comp_login():
-    return render_template('CompLogin.html')
+    return render_template("CompLogin.html", msg="")
 
-@app.route("/company/offers", methods=['GET','POST'])
+
+@app.route("/company/Login", methods=["GET", "POST"])
+def Comp_Login():
+    msg = ""
+    cursor = db_conn.cursor()
+    if request.method == "POST":
+        email = request.form["inputEmail"]
+        password = request.form["inputPassword"]
+        cursor.execute(
+            "SELECT * FROM company WHERE compEmail=%s AND compPassword=%s",
+            (email, password),
+        )
+        record = cursor.fetchone()
+        if record:
+            session["loggedin"] = True
+            session["userid"] = record[0]
+            session["username"] = record[1]
+            return redirect(url_for("comp_offers"))
+        else:
+            msg = "Incorrect email/password.Try again!"
+    return render_template("CompLogin.html", msg=msg)
+
+
+@app.route("/company/offers", methods=["GET", "POST"])
 def comp_offers():
-    return render_template('CompOffers.html')
+    return render_template("CompOffers.html")
 
-#EXAMPLE UPDATE RDS AND S3
-@app.route("/addemp", methods=['GET','POST'])
+
+@app.route("/company/GetOffers", methods=["POST"])
+def comp_get_offers():
+    try:
+        db_conn = connections.Connection(
+            host=customhost,
+            port=3306,
+            user=customuser,
+            password=custompass,
+            db=customdb,
+            cursorclass=cursors.DictCursor,
+        )
+        cursor = db_conn.cursor()
+        if request.method == "POST":
+            draw = request.form["draw"]
+            row = int(request.form["start"])
+            rowperpage = int(request.form["length"])
+            searchValue = request.form["search[value]"]
+            compID = session["userid"]
+
+            ## Total number of records without filtering
+            cursor.execute("SELECT count(*) as allcount from offer WHERE compID=%s", compID)
+            rsallcount = cursor.fetchone()
+            totalRecords = rsallcount["allcount"]
+
+            ## Total number of records with filtering
+            likeString = "%" + searchValue + "%"
+            cursor.execute(
+                "SELECT count(*) as allcount from offer WHERE (position LIKE %s OR location LIKE %s OR prerequisite LIKE %s OR language LIKE %s OR allowance LIKE %s) AND compID=%s",
+                (likeString, likeString, likeString, likeString, likeString, compID),
+            )
+            rsallcount = cursor.fetchone()
+            totalRecordwithFilter = rsallcount["allcount"]
+
+            ## Fetch records
+            if searchValue == "":
+                cursor.execute(
+                    "SELECT * FROM offer WHERE compID=%s ORDER BY offerStatus asc, datePosted desc limit %s, %s;",
+                    (compID, row, rowperpage),
+                )
+                offerlist = cursor.fetchall()
+            else:
+                cursor.execute(
+                    "SELECT * FROM offer WHERE (position LIKE %s OR location LIKE %s OR prerequisite LIKE %s OR language LIKE %s OR allowance LIKE %s) AND compID=%s ORDER BY offerStatus asc, datePosted desc",
+                    (likeString, likeString, likeString, likeString, likeString, compID),
+                )
+                offerlist = cursor.fetchall()
+            
+            data = []
+            for row in offerlist:
+                print(row["offerID"])
+                data.append(
+                    {
+                        "offerID": row["offerID"],
+                        "datePosted": row["datePosted"].strftime("%d-%m-%Y %I:%M:%S %p"),
+                        "position": row["position"],
+                        "allowance": row["allowance"],
+                        "offerStatus": row["offerStatus"],
+                    }
+                )
+                
+            response = {
+                "draw": draw,
+                "iTotalRecords": totalRecords,
+                "iTotalDisplayRecords": totalRecordwithFilter,
+                "aaData": data,
+            }
+            return jsonify(response)
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+
+
+@app.route("/company/addoffer", methods=["GET", "POST"])
+def comp_add_offer():
+    return render_template("CompAddOffer.html")
+
+
+@app.route("/company/AddOffer", methods=["GET", "POST"])
+def Comp_Add_Offer():
+    position = request.form["inputPosition"]
+    allowance = request.form["inputAllowance"]
+    duration = request.form["inputDuration"]
+    prerequisite = request.form["inputPrerequisite"]
+    language = request.form["inputLanguage"]
+    location = request.form["inputLocation"]
+    description = request.form["inputDescription"]
+    datePosted = datetime.now()
+    offerStatus = "active"
+    compID = session["userid"]
+
+    insert_sql = "INSERT INTO offer (position, allowance, duration, prerequisite, language, location, description, datePosted, offerStatus, compID)VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    cursor = db_conn.cursor()
+
+    try:
+        cursor.execute(
+            insert_sql,
+            (
+                position,
+                allowance,
+                duration,
+                prerequisite,
+                language,
+                location,
+                description,
+                datePosted,
+                offerStatus,
+                compID,
+            ),
+        )
+        db_conn.commit()
+
+    except Exception as e:
+        return str(e)
+
+    finally:
+        cursor.close()
+
+    return redirect(url_for("comp_offers"))
+
+
+# EXAMPLE UPDATE RDS AND S3
+@app.route("/addemp", methods=["GET", "POST"])
 def AddEmp():
-    emp_id = request.form['emp_id']
-    first_name = request.form['first_name']
-    last_name = request.form['last_name']
-    pri_skill = request.form['pri_skill']
-    location = request.form['location']
-    emp_image_file = request.files['emp_image_file']
+    emp_id = request.form["emp_id"]
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    pri_skill = request.form["pri_skill"]
+    location = request.form["location"]
+    emp_image_file = request.files["emp_image_file"]
 
     insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
     cursor = db_conn.cursor()
@@ -118,23 +290,26 @@ def AddEmp():
         emp_name = "" + first_name + " " + last_name
         # Uplaod image file in S3 #
         emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
-        s3 = boto3.resource('s3')
+        s3 = boto3.resource("s3")
 
         try:
             print("Data inserted in MySQL RDS... uploading image to S3...")
-            s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
-            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
-            s3_location = (bucket_location['LocationConstraint'])
+            s3.Bucket(custombucket).put_object(
+                Key=emp_image_file_name_in_s3, Body=emp_image_file
+            )
+            bucket_location = boto3.client("s3").get_bucket_location(
+                Bucket=custombucket
+            )
+            s3_location = bucket_location["LocationConstraint"]
 
             if s3_location is None:
-                s3_location = ''
+                s3_location = ""
             else:
-                s3_location = '-' + s3_location
+                s3_location = "-" + s3_location
 
             object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
-                s3_location,
-                custombucket,
-                emp_image_file_name_in_s3)
+                s3_location, custombucket, emp_image_file_name_in_s3
+            )
 
         except Exception as e:
             return str(e)
@@ -143,7 +318,8 @@ def AddEmp():
         cursor.close()
 
     print("all modification done...")
-    return render_template('AddEmpOutput.html', name=emp_name)
+    return render_template("AddEmpOutput.html", name=emp_name)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80, debug=True)
