dfe56d714b488535db128b2afcff5a8f3ea17229
WalterDrinkWater <qingshui9362@gmail.com>
2023-09-17 00:18:00 +0800

    da

--- app.py
+++ app.py
@@ -119,6 +119,14 @@ def Comp_Register():
 
     return redirect("/")
 
+@app.route('/previewImg/<file>', methods=['GET'])
+def preview(filename):
+    if request.method == 'GET':
+        s3 = boto3.resource('s3')
+        file = s3.Object(custombucket, filename).get()
+        response = make_response(file['Body'].read())
+        response.headers['Content-Type'] = 'application/pdf'
+        return response
 
 @app.route("/company/login", methods=['GET','POST'])
 def comp_login():
