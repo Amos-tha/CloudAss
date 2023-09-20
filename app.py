058bbc65883ba0fd45650be6adcc3e5c5c1b922a
Amos-tha <127003574+Amos-tha@users.noreply.github.com>
2023-09-20 21:50:21 +0800

    upate

--- app.py
+++ app.py
@@ -341,5 +341,62 @@ def stud_view_details():
 
     return render_template('StudentDetails.html', studDetails = studDetails)
 
+@app.route("/student/studUpdate", methods=['GET','POST'])
+def stud_update():
+    if request.method == "POST":
+        studID = request.form['inputstudID']
+        studUniEmail = request.form['inputUniEmail']
+        studLevel = request.form['inputLevel']
+        studProgramme = request.form['inputProgramme']
+        studTutGrp = request.form['inputTutGrp']
+        CGPA = request.form['inputCGPA']
+        superVisorName = request.form['inputSupervisor']
+
+        studName = request.form['inputName']
+        studIC = request.form['inputIC']
+        studGender = request.form['inputGender']
+        studPersonalEmail = request.form['inputPersonalEmail']
+        studPhone = request.form['inputPhone']
+        studAddress = request.form['inputAddress']
+
+        # studResume = request.files["inputResume"]
+
+        get_supervisorid_sql = "SELECT superVisorID FROM supervisor WHERE superVisorName = (%s)"
+        insert_sql = "INSERT INTO student (studID, studName, studIC, studPhone, studGender, studUniEmail, studPersonalEmail, studAddress, studLevel, studProgramme, studTutGrp, CGPA, supervisorID)VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        cursor = db_conn.cursor()
+
+        # if studResume.filename == "":
+        #     return "Please upload your resume!"
+
+        try:
+            cursor.execute(get_supervisorid_sql, superVisorName)
+            superVisorID = cursor.fetchone()
+            cursor.execute(insert_sql, (studID, studName, studIC, studPhone, studGender, studUniEmail, studPersonalEmail, studAddress, studLevel, studProgramme, studTutGrp, CGPA, superVisorID))
+            db_conn.commit()
+
+            resume_file = "stud-id-" + str(studID) + "_resume.pdf"
+            s3 = boto3.resource("s3")
+
+            # try:
+            #     print("Data inserted in MySQL RDS... uploading resume to S3...")
+            #     s3.Bucket(custombucket).put_object(Key=resume_file, Body=studResume)
+            #     bucket_location = boto3.client("s3").get_bucket_location(
+            #         Bucket=custombucket
+            #     )
+            #     s3_location = bucket_location["LocationConstraint"]
+
+            #     if s3_location is None:
+            #         s3_location = ""
+            #     else:
+            #         s3_location = "-" + s3_location
+
+            # except Exception as e:
+            #     return str(e)
+
+        finally:
+            cursor.close()
+
+    return redirect(url_for("stud_view_details"))
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
